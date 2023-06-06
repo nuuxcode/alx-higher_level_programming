fa85094df8891e84f5fc700785f0b6d08de02215
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-06 11:28:20 +0100

    fix negative case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = int(str(number)[-1])
+if number < 0:
+    last_digit *= -1
 if last_digit == 0:
     second_string = f"and is 0"
 if last_digit < 6:
