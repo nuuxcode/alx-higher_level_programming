f373efb2dd51a265a95e3f3c09bd335709eedae9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-06 11:43:04 +0100

    change the way i get last digit to the module 10

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
+last_digit = number % 10
 if number < 0:
     last_digit *= -1
 if last_digit == 0:
