5882135edce8875adbec474e2d7ba93ffe4cbd71
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-06 12:50:43 +0100

    Task5 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print("{} = 0x{:x}".format(num, num))
+for num in range(0, 100):
+    between = ", "
+    if num == 99:
+        between = ""
+    print("{:02d}".format(num),end = between)
