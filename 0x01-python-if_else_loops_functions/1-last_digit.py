f6de516647bebbbd355b2f4c83460eaa750775b7
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-06 11:46:38 +0100

    fix case -98

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+
 if number < 0:
+    last_digit = -number % 10
     last_digit *= -1
+else:
+    last_digit = number % 10
 if last_digit == 0:
     second_string = f"and is 0"
 elif last_digit < 6:
