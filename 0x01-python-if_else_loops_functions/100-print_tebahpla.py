e4eedd72376a83f7052faf66e862904c63056e8f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-06 22:29:56 +0100

    Task14 Smile in the mirror

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newstring = ""
+    for i in str:
+        if 'a' <= i <= 'z':
+            newstring += chr((ord(i) - ord('a')) + ord('A'))
+        else:
+            newstring += i
+    return newstring
+
+for alpha in range(ord('z'), ord('a')+-1, -1):
+    if alpha % 2 == 1:
+        alpha = ord(uppercase(chr(alpha)))
+    print("{}".format(chr(alpha)), end='')
