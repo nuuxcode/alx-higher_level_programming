ac0745448c078b979f14bddc978f80d762de6c7a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-08 15:17:36 +0100

    fix pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if len == 1:
         print("0 arguments.")
     elif len > 1:
-        print("{:d} argument{:s}".format(len-1,"s:" if len > 2 else ":"))
+        print("{:d} argument{:s}".format(len-1, "s:" if len > 2 else ":"))
         for i in range(1, len):
             print("{:d}: {:s}".format(i, argv[i]))
