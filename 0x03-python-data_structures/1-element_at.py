53982d02e59e7deffd9fa955af0efc8d6ba667e0
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-09 11:21:17 +0100

    Task4 Replace in a copy, improve code solution for task1,task2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,4 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    for i in range(len(my_list)):
-        if i == idx:
-            return my_list[i]
+    return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,5 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    for i in range(len(my_list)):
-        if i == idx:
-            my_list[i] = element
-            return my_list
+    my_list[idx] = element
+    return my_list
