54b655ddc1543f4e904ac3683ccc1ff0f4f3986f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-10 09:23:38 +0100

    fix pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	for ele in matrix:
-		for ele2 in ele:
-			print("{:d}".format(ele2),end="" if ele[len(ele)-1] == ele2 else " ")
-		print("".format())
+    for ele in matrix:
+        for ele2 in ele:
+            print("{:d}".format(ele2),
+                  end="" if ele[len(ele)-1] == ele2 else " ")
+        print("".format())
