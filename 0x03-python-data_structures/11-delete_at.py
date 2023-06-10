16e46b280fc4a3f05d6cd77e8f9800a6d48160a1
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-10 12:15:09 +0100

    remve len for return

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list is None or len(my_list) == 0:
+    if my_list is None:
         return
     if idx < 0 or idx >= len(my_list):
         return my_list
