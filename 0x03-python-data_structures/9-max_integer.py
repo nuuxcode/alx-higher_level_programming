bcb9669398ddf13bce7d87d3a55217dd71274b82
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-10 11:04:01 +0100

    Task9 Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None or len(my_list) == 0:
+        return None
+    max = 0
+    for num in my_list:
+        if max < num:
+            max = num
+    return max
