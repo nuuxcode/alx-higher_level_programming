fc517e89eef57d535e9715f21306d42af73632f3
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-10 10:28:49 +0100

    Optimise #2 code task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-def check_tuple(tuple=()):
-    if len(tuple) == 0:
-        return 0, 0
-    if len(tuple) == 1:
-        return tuple[0], 0
-    return tuple
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = check_tuple(tuple_a)
-    b = check_tuple(tuple_b)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
     return a[0]+b[0], a[1]+b[1]
