8c0dd5dadd724de5f033be5901954c137a47c8ac
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-10 10:48:02 +0100

    optimise code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return 0, None
-    if len(sentence) == 0:
+    if sentence is None or len(sentence) == 0:
         return 0, None
     return len(sentence), sentence[0]
