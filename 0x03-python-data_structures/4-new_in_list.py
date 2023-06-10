058ee022d303f8eac6989dcd13f9502f6101e2e3
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-10 09:12:59 +0100

    Task4 have to return copy of orginal

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-	if idx < 0 or idx >= len(my_list):
-		return my_list
-	new_list = []
-	for i in range(len(my_list)):
-		new_list.append(my_list[i])
-		if i == idx:
-			new_list[i] = element
-	return new_list
+    new_list = []
+    new_list = new_list + my_list
+    if idx > 0 and idx < len(my_list):
+        new_list[idx] = element
+    return new_list
