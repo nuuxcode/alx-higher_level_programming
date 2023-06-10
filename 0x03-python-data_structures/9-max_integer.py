15e3659c6e88c586fd55a899c9b2021db43745bd
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-10 11:16:16 +0100

    change max to index0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list is None or len(my_list) == 0:
-        return None
-    max = 0
+        return
+    max = my_list[0]
     for num in my_list:
         if max < num:
             max = num
