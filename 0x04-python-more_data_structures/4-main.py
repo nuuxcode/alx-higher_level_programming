774b639e0dbab0a6dca286af3146709bb93dce68
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-14 22:26:28 +0100

    Task4 fix: change union to differenece

--- 0x04-python-more_data_structures/4-main.py
+++ 0x04-python-more_data_structures/4-main.py
@@ -2,6 +2,6 @@
 only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
 
 set_1 = { "Python", "C", "Javascript" }
-set_2 = {  }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
 od_set = only_diff_elements(set_1, set_2)
 print(sorted(list(od_set)))
