4d90e534d1ace33427d8e405624e936f17e47ea4
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-14 13:18:38 +0100

    Task0 fix wrong len from matrix to row

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -4,6 +4,7 @@ square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
+    [7, 8, 9],
     [7, 8, 9]
 ]
 
--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # return [[row[i] for row in matrix] for i in range(3)]
-    if not matrix or not matrix[0]:
-        return
-    return [[row[i]**2 for i in range(len(matrix))] for row in matrix]
+    return [[row[i]**2 for i in range(len(row))] for row in matrix]
