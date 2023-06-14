00a45ffa2da0e72486e8243afd184ae04e544eb9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-14 13:15:41 +0100

    Task0 check if matrix empty

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # return [[row[i] for row in matrix] for i in range(3)]
+    if not matrix or not matrix[0]:
+        return
     return [[row[i]**2 for i in range(len(matrix))] for row in matrix]
