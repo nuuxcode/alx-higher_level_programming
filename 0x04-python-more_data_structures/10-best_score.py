2b56dead3ae9342d2f1750fed1daf4734d33241c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-15 07:55:18 +0100

    Task10 fix: add len for dict and fix pycodestyle

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        return list(dict(sorted(a_dictionary.items(), key=lambda x: (-x[1], x[0]))))[0]
+    if a_dictionary is not None and len(a_dictionary) != 0:
+        return list(dict(sorted(a_dictionary.items(),
+                                key=lambda x: (-x[1], x[0]))))[0]
--- 0x04-python-more_data_structures/10-main.py
+++ 0x04-python-more_data_structures/10-main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 best_score = __import__('10-best_score').best_score
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
+my_dict = { }
+best_key = best_score(my_dict)
+print("Best: {}".format(best_key))
