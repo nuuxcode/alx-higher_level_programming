82b70a97c1fe7a0f7758691ae384c38e4a37bc06
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-15 09:23:18 +0100

    Task12: solution with dict and for loop

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -17,3 +17,9 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "DCCVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "9"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = None
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    return int(roman_string)
+    sum = 0
+    if roman_string is not None:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+        for char in roman_string:
+            if char in roman:
+                sum += roman[char]
+    return sum
