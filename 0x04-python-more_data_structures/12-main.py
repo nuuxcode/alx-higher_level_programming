c195713fd51da361733c4d6fb8703801c50b9fc4
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-15 14:47:31 +0100

    Task12 not finish yet

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -3,23 +3,29 @@
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "I"
+print("{} 1 = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "III"
+print("{} 3 = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "XXI"
+print("{} 21 = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "IV"
+print("{} 4 = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "CXXIV"
+print("{} 124 = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "9"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "89"
+print("{} 0 = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = None
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print("{} 0 = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "XCIX"
+print("{} 99 = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXIX"
+print("{} 89 = {}".format(roman_number, roman_to_int(roman_number)))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     sum = 0
+    index = 0
     if roman_string is not None:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                  'C': 100, 'D': 500, 'M': 1000}
+        listrom = list(roman.keys())
+        length = len(roman_string)
         for char in roman_string:
+            # print("++ "+char)
             if char in roman:
+                # print("+ "+char, roman[char])
+                if char in 'IXC':
+                    if index < length-1:
+                        if listrom[index+1] in 'VXLCDM':
+                            sum = sum + (roman[listrom[index+1]]-roman[char])
                 sum += roman[char]
+            index += 1
     return sum
+
+
+"""
+check if current charachter index is equal to lenght
+"I" can be subtracted from "V" and "X",
+"X" can be subtracted from "L" and "C",
+"C" can be subtracted from "D" and "M".
+However, "V", "L", and "D" cannot be used to represent subtraction.
+"""
