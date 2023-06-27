3951f7337c199dfa33a9c62f017ffab5c5b3cd93
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-27 10:07:37 +0000

    Task3 Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        print("Inside result: {:.1f}".format(a/b))
+        return (a/b)
+    except ZeroDivisionError:
+        print("Inside result: {}".format(None))
+        return None
