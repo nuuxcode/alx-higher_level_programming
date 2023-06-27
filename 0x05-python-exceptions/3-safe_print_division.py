2b3e2895f06093c8101d07723171eab1dfd2ee4c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-06-27 10:16:43 +0000

    Task3 fix: add finally:

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        print("Inside result: {:.1f}".format(a/b))
-        return (a/b)
+        return (a / b)
     except ZeroDivisionError:
-        print("Inside result: {}".format(None))
         return None
+    finally:
+        print("Inside result: {}".format(None if b == 0 else (a / b)))
