e6ac15580523425c25f3509ae2127d432589b16f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-05 08:34:56 +0000

    Task1 fix: change placement in dict

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 Rectangle = __import__('1-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
+my_rectangle = Rectangle()
 print(my_rectangle.__dict__)
 
-my_rectangle.width = 10
-my_rectangle.height = 3
+
 print(my_rectangle.__dict__)
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     def height(self):
         return self.__height
