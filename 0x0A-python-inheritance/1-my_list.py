8068cc2905b2d114ab603a61f3689d9e2685de87
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-10 10:04:57 +0000

    Task1 My lists

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+    prints listsorted
+"""
+
+
 class MyList(list):
+    """
+    sort function
+    """
+
     def print_sorted(self):
         new_list = self[:]
         new_list.sort()
