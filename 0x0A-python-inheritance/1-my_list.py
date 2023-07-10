66f057aecf31ae3f68b23b23af4b86fce39dbf8e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-10 11:48:17 +0000

    Task1 trying to find cases

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,9 +10,6 @@ class MyList(list):
     """
 
     def print_sorted(self):
-        """
-        sorted func
-        """
         new_list = self[:]
         new_list.sort()
         print(new_list)
