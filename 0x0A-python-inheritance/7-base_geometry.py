c016e61072a35683fd5bb0ff91e16c9722c8cd05
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-10 15:39:48 +0000

    Task7 Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" geometry module """
+
+
+class BaseGeometry:
+    """getmetry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
