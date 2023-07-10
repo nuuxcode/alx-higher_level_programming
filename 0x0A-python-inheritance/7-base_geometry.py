e4075f2ca7cf9a55831cc0d724b02400a4d6cb5d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-10 19:38:46 +0000

    Task7 trying find new cases

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,11 +4,14 @@
 
 class BaseGeometry:
     """getmetry class"""
+    pass
 
     def area(self):
+        """ the area """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """ validation """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
