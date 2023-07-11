62193776782e8bdc20d9b12c84c028fb34d6e80e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-11 02:01:31 +0000

    Task12 My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class MyInt(int):
+    """class"""
+    def __new__(cls, num):
+        return super().__new__(cls, num)
+
+    def __init__(self, num):
+        self.num = num
+
+    def __eq__(self, num2):
+        return self.num == num2
+
+    def __ne__(self, num2):
+        return self.num != num2
