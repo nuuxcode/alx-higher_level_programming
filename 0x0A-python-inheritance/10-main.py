a1f2e881a6eac3c55c57bc6f2088e04ee14c6405
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-11 01:38:02 +0000

    Task10 Square #1

---
+++ 0x0A-python-inheritance/10-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+Square = __import__('10-square').Square
+
+s = Square(13)
+print(dir(s))
+print(s)
+print(s.area())
---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class """
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
