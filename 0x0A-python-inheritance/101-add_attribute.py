196a64c72a3f0618401a3000f0a4cb40df5fb856
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-11 02:42:18 +0000

    Task13 add more type

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,6 @@
 
 def add_attribute(obj, atr, value):
     """function """
-    if type(obj) in (list, dict, set, str, int, float, complex, frozenset):
+    if type(obj) in (list, dict, set, str, int, float, complex, frozenset, tuple):
         raise TypeError("can't add new attribute")
     setattr(obj, atr, value)
