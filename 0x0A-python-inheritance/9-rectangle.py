3ad0bf6a8adbc7f515778b5e86bd82896b3bf840
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-11 01:06:52 +0000

    Task9 Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" rectangle module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ rectangle """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        """ area """
+        return self.__height * self.__width
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
