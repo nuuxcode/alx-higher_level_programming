05635e3a827dc8e872d382815874e3590d99e352
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-11 02:30:03 +0000

    Task13 add doc

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" module """
 
 
 def add_attribute(obj, atr, value):
+    """function """
     if type(obj) in (list, dict, set, str, int):
         raise TypeError("can't add new attribute")
     setattr(obj, atr, value)
