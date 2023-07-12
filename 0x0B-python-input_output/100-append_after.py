a9c76ac366941a166d53ed21be2e7d55fd561ab7
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-12 22:53:57 +0000

    Task13 Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ module """
 
+
 def append_after(filename="", search_string="", new_string=""):
-    ip=1
-    with open(filename ,'r') as f:
+    ip = 1
+    with open(filename, 'r') as f:
         lines = f.readlines()
-        new_list=lines[:]
+        new_list = lines[:]
         for i, string in enumerate(lines):
-                print(search_string+str(i))
+            if search_string in string:
                 new_list.insert(i+ip, new_string)
-                ip+=1
+                ip += 1
                 continue
 
     with open(filename, 'w') as f:
