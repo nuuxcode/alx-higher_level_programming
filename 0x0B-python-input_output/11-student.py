9a90ae3e009c09ff6bbf96369f54330f38658acd
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-12 02:10:38 +0000

    Task11 Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module """
+
+
+class Student:
+    """ class """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {key: self.__dict__[key]
+                for key in attrs if key in self.__dict__}
+
+    def reload_from_json(self, json):
+        self.__dict__.update(json)
