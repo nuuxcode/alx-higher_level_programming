5b4c83635a19679fb6bbda84f41dc30bb60864c9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-13 05:43:45 +0000

    Task14 Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module """
+
+import sys
+counter = 0
+data = {"File size": 0, "200": 0, "301": 0, "400": 0,
+        "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+globaldata = []
+for line in sys.stdin:
+    if counter == 10:
+        counter = 0
+        for key, value in globaldata[0].items():
+            if value == 0:
+                continue
+            print(key, ":", value)
+    counter += 1
+    if counter == 1:
+        globaldata.insert(0, data.copy())
+    line = line.rstrip()
+    globaldata[0]["File size"] += int(line.split()[-1])
+    globaldata[0][line.split()[-2]] += 1
