91a134ba0a9bf23a15d89ba4098e866c6d53d478
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-16 15:51:51 +0000

    Task15 Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ module doc """
+import json
 
 
 class Base:
@@ -12,3 +13,8 @@ class Base:
         if id is None:
             self.__class__.__nb_objects += 1
             self.id = self.__class__.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
