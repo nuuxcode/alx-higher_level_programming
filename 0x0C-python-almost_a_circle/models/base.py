4f5e39abd3cd4227fc36f951b9f86ece5175fd5d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-17 21:11:51 +0000

    Task15 testing

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ func doc """
-        if (list_dictionaries is None or type(list_dictionaries) != list or len(list_dictionaries) == 0):
+        if list_dictionaries is None or type(list_dictionaries) != list\
+                or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
