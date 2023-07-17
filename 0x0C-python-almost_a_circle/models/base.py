a7dc754c2bd0ce28b3f954fbe7a2b1fc9f5be4f5
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-17 21:08:46 +0000

    Task15 add check for list type

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,8 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ func doc """
-        if (list_dictionaries is None or len(list_dictionaries) == 0 or
-                type(list_dictionaries) != list):
+        if (list_dictionaries is None or type(list_dictionaries) != list or len(list_dictionaries) == 0):
             return "[]"
         return json.dumps(list_dictionaries)
 
