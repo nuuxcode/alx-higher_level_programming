a365cf159c147715ba95d93b497f5b6559518a9c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-17 18:28:45 +0000

    Task2 Test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,21 +7,15 @@ from models.rectangle import Rectangle
 class RectangleTestCase(unittest.TestCase):
     """ test class for rectangle """
 
-    def test_Rectangle_id_increment(self):
-        b1 = Rectangle()
-        self.assertEqual(b1.id, 1)
+    def test_Rectangle(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
 
-    def test_Rectangle_id_increment_multiple(self):
-        b2 = Rectangle()
-        b3 = Rectangle()
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
 
-    def test_Rectangle_id_with_parameter(self):
-        b4 = Rectangle(12)
-        b5 = Rectangle()
-        self.assertEqual(b4.id, 12)
-        self.assertEqual(b5.id, 4)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
 
 
 if __name__ == '__main__':
