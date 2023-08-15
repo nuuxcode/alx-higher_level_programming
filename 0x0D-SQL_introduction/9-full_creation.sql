3bc1b1d840faf52e066a84769c2825075373ecb1
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-15 21:23:44 +0000

    Task9 Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- creates a table second_table in the database hbtn_0c_0
+-- cat 9-full_creation.sql | sudo mysql -hlocalhost -uroot -p hbtn_0c_0
+CREATE TABLE
+    IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+INSERT INTO
+    second_table (id, name, score)
+VALUES
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
