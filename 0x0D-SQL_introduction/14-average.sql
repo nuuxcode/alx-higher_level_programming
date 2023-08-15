e646bda9a37957019bf4e8d32c2d3e60010190e0
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-15 22:08:56 +0000

    Task14 add : AS average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- computes the score average of all records in the table second_table
 -- cat 14-average.sql | sudo mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT AVG(score)
+SELECT AVG(score) AS average
 FROM second_table;
