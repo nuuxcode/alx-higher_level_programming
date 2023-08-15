528593988090aecb52e5b9bf8a040820118b3e97
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-15 22:22:04 +0000

    Task15 add sorting

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,11 @@
 -- lists the number of records with the same score in the table second_table
 -- cat 15-groups.sql | sudo mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT score, COUNT(*) AS number
-FROM second_table
-GROUP BY score
+SELECT
+    score,
+    COUNT(*) AS number
+FROM
+    second_table
+GROUP BY
+    score
+ORDER BY
+    number DESC;
