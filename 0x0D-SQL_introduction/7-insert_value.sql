c65db755780edb0b2bbecdb7837081c391959073
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-15 20:56:48 +0000

    Task7 First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- inserts a new row in the table first_table
+-- cat 7-insert_value.sql | sudo mysql -hlocalhost -uroot -p hbtn_0c_0
+INSERT INTO
+    first_table (id, name)
+VALUES
+    (89, "Best School");
