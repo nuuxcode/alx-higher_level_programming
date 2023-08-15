251b87cc116bd44ab6e55268062d8be143547797
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-15 20:33:52 +0000

    Task5 Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints the full description of the table first_table from the database hbtn_0c_0
+-- cat 5-full_table.sql | sudo mysql -hlocalhost -uroot -p hbtn_0c_0
+SHOW CREATE TABLE first_table;
