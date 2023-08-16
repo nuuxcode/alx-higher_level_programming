8ef07b8b3f99197af8a98047500ed5e3cdf43022
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 22:34:40 +0000

    Task13 Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- cat 13-count_shows_by_genre.sql | sudo mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+
+SELECT c.name, COUNT(*) AS number_of_shows
+FROM tv_genres c
+LEFT JOIN tv_show_genres s ON c.id = s.genre_id
+GROUP BY c.name;
