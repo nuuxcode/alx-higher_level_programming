59d4fa3c4949214679d4866bd02448aacc4b19d2
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 09:39:38 +0000

    Task0 fix: remove comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 
 -- cat 0-privileges.sql | sudo mysql -hlocalhost -uroot -p
--- echo "CREATE USER 'user_0d_1'@'localhost';" |  sudo mysql -hlocalhost -uroot -p
--- echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" |  sudo mysql -hlocalhost -uroot -p
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
