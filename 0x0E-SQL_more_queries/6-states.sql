e7d016bb5d5f9ff05cd63525c2ba6864ffacf1c1
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 07:59:48 +0000

    Task6 States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+-- cat 6-states.sql | sudo mysql -hlocalhost -uroot -p
+-- echo 'INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");' | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
+-- echo 'SELECT * FROM states;' | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE
+    IF NOT EXISTS states (
+        id INT AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+        name VARCHAR(256)
+    )
