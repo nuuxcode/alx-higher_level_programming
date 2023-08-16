cfd4a25c8741edb2c9f4888c7e7650750dfa5bb2
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 23:14:45 +0000

    Task16 fix name

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,12 +1,12 @@
 -- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 -- cat 16-shows_by_genre.sql | sudo mysql -hlocalhost -uroot -p hbtn_0d_tvshows
 SELECT
-    s.title AS show_title,
-    g.name AS genre
+    s.title AS title,
+    g.name AS name
 FROM
     tv_shows s
     JOIN tv_show_genres sg ON s.id = sg.show_id
     JOIN tv_genres g ON sg.genre_id = g.id
 ORDER BY
-    show_title,
-    genre ASC;
+    title,
+    name ASC;
