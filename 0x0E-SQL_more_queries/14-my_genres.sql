edef7f0a7ca7e59c5d72fb11f632dd12f085e8da
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 22:58:15 +0000

    Task14 My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- cat 14-my_genres.sql | sudo mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+SELECT g.name AS genre
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
+WHERE s.title = 'Dexter';
