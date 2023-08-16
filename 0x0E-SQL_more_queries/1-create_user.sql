ee2da28fbae24d70a59607c43bc08d16421de0c4
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 10:19:38 +0000

    Task1 fix: adding native password way

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- creates the MySQL server user user_0d_1
 -- cat 1-create_user.sql | sudo mysql -hlocalhost -uroot -p
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
