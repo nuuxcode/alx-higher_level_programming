a75154a7ee9def7c60605c91431445f744ecd0e9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 09:47:28 +0000

    Task2 fix: typo

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 -- cat 2-create_read_user.sql | sudo mysql -hlocalhost -uroot -p
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
