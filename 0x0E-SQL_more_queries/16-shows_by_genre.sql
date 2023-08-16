ce9910c758453e9ae327d11b399124a9b99d6038
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 23:27:03 +0000

    Task16 add left join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,8 +5,8 @@ SELECT
     g.name AS name
 FROM
     tv_shows s
-    JOIN tv_show_genres sg ON s.id = sg.show_id
-    JOIN tv_genres g ON sg.genre_id = g.id
+    LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+    LEFT JOIN tv_genres g ON sg.genre_id = g.id
 ORDER BY
     title,
     name ASC;
