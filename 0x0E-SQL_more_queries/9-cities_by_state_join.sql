06a3f662b03b2b676a383328306d3f57b125cae4
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-08-16 12:34:44 +0000

    Task9 Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- lists all cities contained in the database hbtn_0d_usa
+-- echo 'SELECT * FROM states;' | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
+-- echo 'SELECT * FROM cities;' | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
+-- cat 9-cities_by_state_join.sql | sudo mysql -hlocalhost -uroot -p hbtn_0d_usa
+
+SELECT id, name, (SELECT name FROM states WHERE id = cities.state_id) AS ""
+FROM cities;
+
