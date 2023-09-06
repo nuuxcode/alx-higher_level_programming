95ba0fd167a4486437a3db5ec0c3e8f30801e2a2
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-06 17:00:36 +0000

    Task7 I love C, update task5 with isNAN

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const Str = 'C is fun';
+let x = parseInt(argv[2], 10);
+if (!isNaN(x)) {
+    while (x > 0) {
+        console.log(Str);
+        x--;
+    }
+}
+else console.log('Missing number of occurrences');
