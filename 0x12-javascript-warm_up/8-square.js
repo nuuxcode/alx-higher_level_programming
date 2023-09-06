c3e7ace1e2ae4fad5c3e96ab5a669463cc7f26c5
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-06 17:25:07 +0000

    Fix semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const Str = 'X';
-let x = parseInt(argv[2], 10);
+const x = parseInt(argv[2], 10);
 if (!isNaN(x)) {
-    for (let i = 0; i < x; i++) {
-        console.log(Str.repeat(x));
-    }
-}
-else console.log('Missing size');
+  for (let i = 0; i < x; i++) {
+    console.log(Str.repeat(x));
+  }
+} else console.log('Missing size');
