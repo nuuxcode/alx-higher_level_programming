1fe957f075e184a55d80b6afef40c89d3a60a74c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-06 17:23:11 +0000

    Task8 Square with repeat()

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const Str = 'X';
+let x = parseInt(argv[2], 10);
+if (!isNaN(x)) {
+    for (let i = 0; i < x; i++) {
+        console.log(Str.repeat(x));
+    }
+}
+else console.log('Missing size');
