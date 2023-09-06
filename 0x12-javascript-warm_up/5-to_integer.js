be6a4926d02cf566e72f8ef0e3450c23580c4ca3
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-06 16:39:02 +0000

    Task5 An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const myNumber = 'My number: ';
+if (parseInt(argv[2], 10)) console.log(myNumber + parseInt(argv[2], 10));
+else console.log('Not a number');
