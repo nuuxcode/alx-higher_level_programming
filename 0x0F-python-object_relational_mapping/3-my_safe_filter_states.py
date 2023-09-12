7d459d208127edccb0bb363dc43e7a5e150c6064
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-12 18:48:23 +0100

    Task3 SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db_host = "localhost"
+    db_user = sys.argv[1]  # "your_username"
+    db_password = sys.argv[2]  # "your_password"
+    db_name = sys.argv[3]  # "your_database_name"
+    port = 3306
+    state_name = MySQLdb.escape_string(sys.argv[4])  # "your_database_name"
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    params = (state_name,)
+    db = MySQLdb.connect(
+        host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
+    )
+    cursor = db.cursor()
+
+    cursor.execute(query, params)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    db.close()
