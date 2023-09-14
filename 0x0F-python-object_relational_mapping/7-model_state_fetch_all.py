b45e1d214b71d04d3f09d53b1d4bb4c3c2d1a932
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-14 03:07:35 +0100

    Task7 All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        states = connection.execute(select([State]).order_by(State.id.asc()))
+        for state in states:
+            print(f"{state.id}: {state.name}")
+
+    engine.dispose()
