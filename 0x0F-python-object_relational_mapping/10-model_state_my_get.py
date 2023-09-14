523abab10359b2a4eca5459982583d43a26e84da
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-14 04:43:42 +0100

    Task10  Get a state

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select, text
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    state_name = sys.argv[4]
+    with engine.connect() as connection:
+        query = select([State]) \
+            .where(text("name = :name").bindparams(name=state_name))
+        states = list(connection.execute(query, state_name=state_name))
+        if states:
+            for state in states:
+                print(state[0])
+        else:
+            print("Nothing")
+
+    engine.dispose()
