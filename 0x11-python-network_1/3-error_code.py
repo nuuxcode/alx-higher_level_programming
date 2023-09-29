ff95b3196bb44b946f7a05199d734ff43a179a72
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-29 16:29:31 +0100

    Task3 forget param

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ module doc """
 import urllib.request
-
+import sys
 
 if __name__ == "__main__":
     try:
-        url = "https://alx-intranet.hbtn.io/status"
+        url = sys.argv[1]
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
