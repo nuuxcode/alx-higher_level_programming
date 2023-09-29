a302fc80d157f2af586c83f73c8ce32fafef08fc
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-29 02:13:55 +0100

    Task6 testing without middle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,11 +9,6 @@ def find_peak(list_of_integers):
         r = len(list_of_integers) - 1
         while lf < r:
             m = (lf + r) // 2
-            if (
-                list_of_integers[m] > list_of_integers[m - 1]
-                and list_of_integers[m] > list_of_integers[m + 1]
-            ):
-                return list_of_integers[m]
             if list_of_integers[m] > list_of_integers[m + 1]:
                 r = m
             else:
