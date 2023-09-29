47345f79e9f38f7cae8daa74f8649b43a4dbfe29
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-29 17:50:29 +0100

    Task7 Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,9 +4,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    try:
-        url = sys.argv[1]
-        res = requests.get(url)
-        print(res.text)
-    except requests.exceptions.RequestException as e:
-        print(f"Error code: {e}")
+    url = sys.argv[1]
+    res = requests.get(url)
+    if (res.status_code >= 400):
+        print(f"Error code: {res.status_code}")
+        exit()
+    print(res.text)
