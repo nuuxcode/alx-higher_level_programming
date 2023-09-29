d6a8a904b1b17f93b51acf747f74cb1deafb05b9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-29 02:13:15 +0100

    Task6 Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """ module doc """
 
+
 def find_peak(list_of_integers):
     """def doc"""
     if list_of_integers:
-        l=0
-        r=len(list_of_integers) - 1
-        while l < r:
-            m = (l + r) // 2
-            if list_of_integers[m] > list_of_integers[m - 1] and list_of_integers[m] > list_of_integers[m + 1]:
+        lf = 0
+        r = len(list_of_integers) - 1
+        while lf < r:
+            m = (lf + r) // 2
+            if (
+                list_of_integers[m] > list_of_integers[m - 1]
+                and list_of_integers[m] > list_of_integers[m + 1]
+            ):
                 return list_of_integers[m]
             if list_of_integers[m] > list_of_integers[m + 1]:
                 r = m
             else:
-                l = m + 1
-        return list_of_integers[l]
+                lf = m + 1
+        return list_of_integers[lf]
