825a7cdf6d88b68826cd5b44cff5e49d35c36d3c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-24 12:08:47 +0100

    Task6 fix last check

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,10 +6,10 @@ request(url, { json: true }, (err, res, body) => {
   if (err) console.log(err);
   const results = {};
   for (const i in body) {
-    if (results[body[i].userId] === undefined) {
-      results[body[i].userId] = 0;
-    }
     if (body[i].completed === true) {
+      if (results[body[i].userId] === undefined) {
+        results[body[i].userId] = 0;
+      }
       results[body[i].userId] += 1;
     }
   }
