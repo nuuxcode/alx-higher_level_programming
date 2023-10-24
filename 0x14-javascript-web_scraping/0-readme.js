271458dbf5609fd62c8ba30987c159d8ba3171cf
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-24 09:59:30 +0100

    Task0 Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+try {
+    const data = fs.readFileSync(file, 'utf8');
+    console.log(data);
+} catch (err) {
+    console.error(err);
+}
