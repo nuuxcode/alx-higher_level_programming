ceb942d694e448284d7ccb8d4a4ad47e407199fe
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-24 10:04:23 +0100

    Task0 fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const file = process.argv[2];
 try {
-    const data = fs.readFileSync(file, 'utf8');
-    console.log(data);
+  const data = fs.readFileSync(file, 'utf8');
+  console.log(data);
 } catch (err) {
-    console.error(err);
+  console.error(err);
 }
