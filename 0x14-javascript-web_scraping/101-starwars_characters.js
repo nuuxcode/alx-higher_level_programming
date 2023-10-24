98bff7bd2cfc2204fdea4a12bd51a7a183e01a28
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-24 13:11:16 +0100

    Task8 Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
-const request = require('request-promise');
+const request = require('request');
+const util = require('util');
 const id = process.argv[2];
 
-request(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true }, async (err, res, body) => {
-  if (err) console.log(err);
-  for (const character of body.characters) {
-    try {
-      const char = await request(character, { json: true });
-      console.log(char.name);
-    } catch (err) {
-      console.log(err);
+const prequest = util.promisify(request);
+
+(async () => {
+  try {
+    const body = (await prequest(`https://swapi-api.alx-tools.com/api/films/${id}`, { json: true })).body;
+    for (const url of body.characters) {
+      const body = (await prequest(url, { json: true })).body;
+      console.log(body.name);
     }
+  } catch (err) {
+    console.log(err);
   }
-});
+})();
