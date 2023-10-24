17edff77023e798a5c0affeb70958d5531c430da
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-24 12:19:43 +0100

    Task6 improve code

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,12 +5,9 @@ const url = process.argv[2];
 request(url, { json: true }, (err, res, body) => {
   if (err) console.log(err);
   const results = {};
-  for (const i in body) {
-    if (body[i].completed === true) {
-      if (results[body[i].userId] === undefined) {
-        results[body[i].userId] = 0;
-      }
-      results[body[i].userId] += 1;
+  for (const task of body) {
+    if (task.completed) {
+      results[task.userId] = (results[task.userId] || 0) + 1;
     }
   }
   console.log(results);
